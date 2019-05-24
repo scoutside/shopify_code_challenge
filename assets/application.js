4e6860c40a314390f136ff8a7746ebb395d3245a
Travis Frazier <31898316+travisfrazier@users.noreply.github.com>
2019-05-24 09:00:32 -0700

    Add files via upload

---
+++ assets/application.js
@@ -0,0 +1,10 @@
+// Put your applicaiton javascript here
+// $("#addToCart").click(function(){
+//     $.post('/cart/add.js', {
+//       quantity: 1,
+//       id: 15428214784046,
+//       properties: {
+//         'First name': 'Caroline'
+//       }
+//     });
+//   });
